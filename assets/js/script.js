8f23557c08107633bb1a0ed95fbd72ad9eb6e171
Pablo Embarach <pabloembarach@gmail.com>
2022-06-01 19:06:26 +0000

    Add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,9 @@ function runGame(gameType) {
 
     if (gameType === "addition"){
       displayAdditionQuestion(num1, num2);
-    } else if (gameType === "multiply") {
+    } else if (gameType === "subtract") {
+      displaySubtractQuestion(num1, num2);
+    }  else if (gameType === "multiply") {
       displayMultiplyQuestion(num1, num2);
     } else {
       alert(`Unknown game type: ${gameType}`);
@@ -70,6 +72,8 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
     return [operand1 + operand2, "addition"];
+  } else if (operator === "-") { 
+    return [operand1 - operand2, "subtract"];
   } else if (operator === "x") { 
     return [operand1 * operand2, "multiply"];
   } else {
@@ -102,7 +106,10 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById('operator').textContent = "-";
 
 }
 
